e9a8a5b255d47d980eed29f71dcfe3e2f7ad00b7
A601-12 <mkii.dat@gmail.com>
2020-06-25 12:00:38 +0900

    ボタンを消す

--- cs_062500/Form1.Designer.cs
+++ cs_062500/Form1.Designer.cs
@@ -28,25 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.mybutton1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // mybutton1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(260, 237);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.mybutton1.BackColor = System.Drawing.Color.PowderBlue;
+            this.mybutton1.Font = new System.Drawing.Font("HGP創英角ﾎﾟｯﾌﾟ体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.mybutton1.Location = new System.Drawing.Point(12, 12);
+            this.mybutton1.Name = "mybutton1";
+            this.mybutton1.Size = new System.Drawing.Size(260, 105);
+            this.mybutton1.TabIndex = 0;
+            this.mybutton1.Text = "押すなよ";
+            this.mybutton1.UseVisualStyleBackColor = false;
+            this.mybutton1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.Font = new System.Drawing.Font("HGS創英角ﾎﾟｯﾌﾟ体", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(12, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(260, 108);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "(´・ω・`)";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.mybutton1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,7 +72,8 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button mybutton1;
+        private System.Windows.Forms.Button button2;
 
     }
 }
--- cs_062500/Form1.cs
+++ cs_062500/Form1.cs
@@ -12,6 +12,7 @@ namespace cs_062500
 {
     public partial class Form1 : Form
     {
+        int kaisuu;
         public Form1()
         {
             InitializeComponent();
@@ -19,8 +20,33 @@ namespace cs_062500
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int a = 1;
-            MessageBox.Show(""+ (10/a));
+            mybutton1.Visible = false;
+
+            /*
+            //int a = 1;
+            //MessageBox.Show(""+ (10/a));
+            mybutton1.Enabled = false;
+            button2.Visible = true;
+             */
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*
+            kaisuu += 1;
+            if(kaisuu <= 3){
+                MessageBox.Show("押すなって言ったのに");
+                mybutton1.Text = "本当に押すなよ？";
+            }
+            else
+            {
+                MessageBox.Show("……。");
+                mybutton1.Text = "……。";
+            }
+            mybutton1.Enabled = true;
+            button2.Visible = false;
+             */
         }
     }
 }
