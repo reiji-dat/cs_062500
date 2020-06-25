527c93a15a6a11eb121cb82feba11f876a459ffc
A601-12 <mkii.dat@gmail.com>
2020-06-25 11:15:10 +0900

    エラーの確認

--- cs_062500/Form1.cs
+++ cs_062500/Form1.cs
@@ -16,5 +16,11 @@ namespace cs_062500
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 1;
+            MessageBox.Show(""+ (10/a));
+        }
     }
 }
